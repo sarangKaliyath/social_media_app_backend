199d4113fbdee5a60e07843f38e6c2fea0584500
sarang.kaliyath <sarang.kaliyath@sakhaglobal.com>
2024-06-22 12:28:07 +0530

    changes for swagger

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ require("dotenv").config();
 const app = require("./src");
 const dbConnect = require("./src/config/dbConnect");
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.json');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 const PORT = process.env.PORT || 8080;
 
 dbConnect();
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const AuthRouter = require("./routes/auth.routes");
 
 app.use(express.json());
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../docs/swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/api/signup", SignupRouter);
 
 app.use("/api/auth", AuthRouter)
