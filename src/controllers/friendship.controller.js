3c901a4b8687ffab470da726820485d26aa9d54d
sarang.kaliyath <sarang.kaliyath@sakhaglobal.com>
2024-07-03 21:47:24 +0530

    api to get friends list, accept friends list bug fix

--- src/controllers/friendship.controller.js
+++ src/controllers/friendship.controller.js
@@ -19,7 +19,7 @@ const sendFriendsRequest = async (req, res) => {
         const isRequestAlreadyReceived = user.received.length > 0 && 
             user.received?.filter((el) => el.user.toString() === userToAdd);
             
-        if(isRequestAlreadyReceived) return errorResponse(res, "Request already received", {requestReceived: true});
+        if(isRequestAlreadyReceived) return errorResponse(res, "Request already received", 400, {requestReceived: true});
 
         user.requested.unshift({user : friendToAddId});
         await user.save();
@@ -40,19 +40,19 @@ const acceptFriendsRequest = async (req, res) => {
         const {userIdToAdd} = req.params;
 
         // this is the user who received and will accept the friends request;
-        const currentUser = await PendingRequestSchema.findOne({user: userIdToAdd});
+        const currentUser = await PendingRequestSchema.findOne({user: userId});
 
         // this is the user who sent the friends request;
-        const userToAdd = await PendingRequestSchema.findOne({user: userId});
+        const userToAdd = await PendingRequestSchema.findOne({user: userIdToAdd});
 
         if(!currentUser || !userToAdd) return errorResponse(res, "User not Found!");
 
         if(currentUser.received.length === 0) return errorResponse(res, "No request received!");
 
-        currentUser.received = currentUser.received.filter((el) => el.user?.toString() !== userId);
+        currentUser.received = currentUser.received.filter((el) => el.user?.toString() !== userIdToAdd);
         await currentUser.save();
 
-        userToAdd.requested = userToAdd.requested.filter((el) => el.user?.toString() !== userIdToAdd);
+        userToAdd.requested = userToAdd.requested.filter((el) => el.user?.toString() !== userId);
         await userToAdd.save();
 
         // updating accepted request DB for both users and adding both to each others friends list;
@@ -78,7 +78,7 @@ const getPendingRequestList = async (req, res) => {
 
         const user = await PendingRequestSchema.findOne({user: userId})
         .populate('requested.user')
-        .populate('requested.user');
+        .populate('received.user');
 
         if(!user) return errorResponse(res, "User not found");
 
@@ -93,10 +93,26 @@ const getPendingRequestList = async (req, res) => {
     }
 };
 
+const getFriendsList = async (req, res) => {
+    try {
+
+        const {userId} = req;
+
+        const friendsList = await AcceptedRequestSchema.findOne({user: userId}).populate("friendsList.user");
+
+        res.status(200).json({error: false, message: "List acquired!", friendsList : friendsList || []});
+       
+    } catch (error) {
+        console.log(error);
+        return serverError(res);
+    }
+}
+
 
 
 module.exports = {
     sendFriendsRequest,
     acceptFriendsRequest,
     getPendingRequestList,
+    getFriendsList,
 }
--- src/routes/friendship.route.js
+++ src/routes/friendship.route.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { sendFriendsRequest, acceptFriendsRequest, getPendingRequestList } = require("../controllers/friendship.controller");
+const { sendFriendsRequest, acceptFriendsRequest, getPendingRequestList, getFriendsList } = require("../controllers/friendship.controller");
 const authMiddleWare = require("../middleware/auth.middleware");
 
 router.get("/send/:friendToAddId", authMiddleWare, sendFriendsRequest);
 router.get("/add/:userIdToAdd", authMiddleWare, acceptFriendsRequest);
 router.get("/pending", authMiddleWare, getPendingRequestList);
+router.get("/accepted", authMiddleWare, getFriendsList);
 
 module.exports = router;
