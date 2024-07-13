5a2192c388ae4f84f190bceeb2b604e0ad452b4f
sarang.kaliyath <sarang.kaliyath@sakhaglobal.com>
2024-07-13 15:37:34 +0530

    api to search friends list

--- src/routes/search.routes.js
+++ src/routes/search.routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const {searchUsers}  = require("../controllers/search.controller");
+const {searchUsers, searchFriends}  = require("../controllers/search.controller");
 const authMiddleWare = require("../middleware/auth.middleware");
 
 
 router.get("/:username", authMiddleWare, searchUsers);
+
+router.get("/friends/:username", authMiddleWare, searchFriends);
 
 module.exports = router;
